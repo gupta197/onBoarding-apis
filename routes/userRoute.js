a55ceb3cb964f6927cc1e8553a3eef02b583d642
Vikas Gupta <vg2292097@gmail.com>
2023-04-08 13:34:51 +0530

    Api enable and setup middleware for authication api

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,8 +1,11 @@
 const router = require('express').Router();
 const {setup2fa,updateUserDetail} = require('../controller/userController');
 const auth = require("../middleware/auth");
+// Enable and Disable 2 factor authication
 router.post('/2fa',auth,setup2fa);
+// Get User detail
 router.post('/:id',auth,updateUserDetail);
+
 router.get('/',auth,async(req,res)=>{
     res.send("Welcome to User Page")
 });
