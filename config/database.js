0b68227d9e12975cd43cfebda5c4944b00e95511
gupta197 <vg2292097@gmail.com>
2024-04-04 22:07:46 +0530

    convert promise to async await

--- config/database.js
+++ config/database.js
@@ -2,20 +2,18 @@ const mongoose = require("mongoose");
 
 const { MONGO_URI } = process.env;
 
-exports.connect = () => {
+exports.connect = async () => {
   // Connecting to the database
-  mongoose.set('strictQuery', true);
-  mongoose
-    .connect(MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    })
-    .then(() => {
-      console.log("Successfully connected to database");
-    })
-    .catch((error) => {
-      console.log("database connection failed. exiting now...");
-      console.error(error);
-      // process.exit(1);
-    });
+  try {
+    mongoose.set('strictQuery', true);
+    mongoose
+      .connect(MONGO_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+      })
+    console.log("Successfully connected to database");
+  } catch (error) {
+    console.log("database connection failed. exiting now...");
+    console.error(error);
+  }
 };
